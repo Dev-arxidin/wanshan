334f623e92723d605d6d88b526430808554c2835
Dev-arxidin <tograkK@outlook.com>
2022-04-30 11:33:56 +0800

    Initial commit

---
+++ app/src/main/java/com/cat/wanshan/MainActivity.java
@@ -0,0 +1,28 @@
+package com.cat.wanshan;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.Window;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE); //这个方法必须写在 setContentView 的前面，了解源码的同学应该知道其原因
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreatePanelMenu(int featureId, @NonNull Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu) ;
+        return super.onCreatePanelMenu(featureId, menu);
+    }
+}
